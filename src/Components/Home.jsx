cc4285e4e252bc84c1813cad4818b37c7c9c1aa7
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-01-07 18:23:00 +0300

    Added my image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/.jpg";
+import image from "../images/kevin-mugarura.jpg";
 
 const imageAltText = "Kevin posing for a smily picture.";
 
